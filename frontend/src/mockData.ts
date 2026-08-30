ba82bba374b941b9a386f246e32175d61fb07b0b
Edwin Bucio <edwin.bucio@wsu.edu>
2026-08-30 16:14:11 -0700

    Basic UI

---
+++ frontend/src/mockData.ts
@@ -0,0 +1,9 @@
+import type { Station } from './types'
+
+export const stations: Station[] = [
+  { id: 'pullman', name: 'Pullman', county: 'Whitman' },
+  { id: 'prosser', name: 'Prosser', county: 'Benton' },
+  { id: 'wenatchee', name: 'Wenatchee', county: 'Chelan' },
+  { id: 'mtvernon', name: 'Mount Vernon', county: 'Skagit' },
+  { id: 'wallawalla', name: 'Walla Walla', county: 'Walla Walla' },
+]
