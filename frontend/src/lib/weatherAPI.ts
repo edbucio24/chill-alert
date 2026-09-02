f02d1be9af52596afcbc1745bbe2ea394491d7bc
Edwin Bucio <edwin.bucio@wsu.edu>
2026-09-01 18:36:34 -0700

    Start of current temp trendline

--- frontend/src/lib/weatherAPI.ts
+++ frontend/src/lib/weatherAPI.ts
@@ -52,3 +52,31 @@ export async function fetchLiveWeather(latitude:number,longitude:number):Promise
     forecast,
     }
 }
+
+export interface HourlyPoint{
+    time:string
+    temperature: number
+    dewPoint: number
+    windSpeed: number
+    precipation: number
+}
+
+
+export async function fetchHourlyData(latitude: number, longitude: number): Promise<HourlyPoint[]> {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,dew_point_2m,wind_speed_10m,precipitation&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=auto&past_days=1&forecast_days=1`
+
+  const res = await fetch(url)
+  if (!res.ok) {
+    throw new Error(`Hourly weather API error: ${res.status}`)
+  }
+  const json = await res.json()
+
+  return json.hourly.time.map((t: string, i: number) => ({
+    time: t,
+    temperature: json.hourly.temperature_2m[i],
+    dewPoint: json.hourly.dew_point_2m[i],
+    windSpeed: json.hourly.wind_speed_10m[i],
+    precipitation: json.hourly.precipitation[i],
+}))
+
+}
